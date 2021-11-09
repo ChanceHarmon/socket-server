815f25348ba2a4c329cc28c8e2f99dbc48d3025c
Chance Harmon <henrikillsit@gmail.com>
2021-11-09 09:09:28 -0800

    working through cors issues

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ console.log(server)
 const io = new Server(server, {
   cors: {
     // origin: 'http://localhost:3000',
-    origin: 'https://chances-socket-client.netlify.app/',
+    origin: 'https://chances-socket-client.netlify.app',
     methods: ['GET', 'POST']
   }
 })
